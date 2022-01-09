fa35037697a1408579ad5f2b44c27dca32e06aea
motaharinia <eng.motahari@gmail.com>
2022-01-09 18:08:57 +0330

    remove unused hibernate search dependencies

--- src/main/java/com/motaharinia/msjpautility/document/customcounter/CustomCounterRepository.java
+++ src/main/java/com/motaharinia/msjpautility/document/customcounter/CustomCounterRepository.java
@@ -3,9 +3,6 @@ package com.motaharinia.msjpautility.document.customcounter;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.validation.constraints.NotNull;
-import java.util.Optional;
-
 /**
  *  کلاس ریپازیتوری شمارنده
  */
